67fd2c18fa2e604712cb66ebb6eb25c2f2eb8293
chiegremoryy <chiescode@gmail.com>
2026-01-15 22:31:35 +0700

    add event management features with create, read, update, and delete functionalities

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\EventController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,9 +18,10 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
         // Category Management
         Route::resource('categories', CategoryController::class);
+        // Event Management
+        Route::resource('events', EventController::class);
     });
 });
 
